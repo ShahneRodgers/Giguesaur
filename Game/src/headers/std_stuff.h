1e5fd1b083bfc26b32efbd387444f104b2662507
Ashley Manson <ashley.manson@hotmail.com>
2015-05-15 00:56:14 +1200

    Added Giguesaur game code to the repo, rm ash_main.c

---
+++ Game/src/headers/std_stuff.h
@@ -0,0 +1,17 @@
+/* 
+ File: stdfun.h
+ Author: Ashley Manson
+ Description: Adds some basic functionality to your C programs.
+ */
+
+#ifndef STD_STUFF_H
+#define STD_STUFF_H
+
+#define BTN_ESCAPE 27
+#define BTN_SPACE 32
+
+#define PI 3.14159265
+
+typedef enum {false, true} bool;
+
+#endif
