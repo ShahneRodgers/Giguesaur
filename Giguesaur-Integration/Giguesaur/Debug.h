e8d3f15ca689f1280bfb62a3122b405014a6eec6
Ashley Manson <ashley.manson@hotmail.com>
2015-08-03 15:16:50 +1200

    still broke gee

--- Giguesaur-Integration/Giguesaur/Debug.h
+++ Giguesaur-Integration/Giguesaur/Debug.h
@@ -8,13 +8,13 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
-#define DEBUG_LEVEL 0
+#define DEBUG_LEVEL 1
 
 #define DEBUG_SAY(lvl, string) \
-        do { if (DEBUG_LEVEL > lvl) fprintf(stderr, string); } while(0)
+        do { if (DEBUG_LEVEL >= lvl) fprintf(stderr, string); } while(0)
 
 #define DEBUG_PRINT(lvl, format, ...) \
-    do { if (DEBUG_LEVEL > lvl) fprintf(stderr, format, __VA_ARGS__); } while(0)
+    do { if (DEBUG_LEVEL >= lvl) fprintf(stderr, format, __VA_ARGS__); } while(0)
 
 
 #endif
